d082edd04b556383945dbee3c4d832f4871a8c44
Yudhistira Haryatmaka Santoso <yudhistiraaaa20@gmail.com>
2023-03-29 15:02:59 +0700

    first commit

---
+++ app/src/main/java/org/d3if3050/asesmen1/Tumbuhan.kt
@@ -0,0 +1,9 @@
+package org.d3if3050.asesmen1
+
+data class Tumbuhan(
+    val nama: String,
+    val namaLatin: String,
+    val deskripsi: String,
+    val caraMerawat: String,
+    val imageResId: Int,
+)
