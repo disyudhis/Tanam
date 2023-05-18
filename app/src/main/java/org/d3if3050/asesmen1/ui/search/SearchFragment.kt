64eb0adfa21351059ebd4426965a15499626e701
Yudhistira Haryatmaka Santoso <yudhistiraaaa20@gmail.com>
2023-05-18 21:36:17 +0700

    mengubah tampilan menjadi bentuk card dalam fragment search

--- app/src/main/java/org/d3if3050/asesmen1/ui/search/SearchFragment.kt
+++ app/src/main/java/org/d3if3050/asesmen1/ui/search/SearchFragment.kt
@@ -10,12 +10,9 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.textfield.TextInputLayout
-import kotlinx.coroutines.job
-import org.d3if3050.asesmen1.R
 import org.d3if3050.asesmen1.databinding.SearchFragmentBinding
 import org.d3if3050.asesmen1.db.TanamDb
 import org.d3if3050.asesmen1.db.TanamEntity
-import java.util.*
 
 class SearchFragment : Fragment() {
     private lateinit var binding: SearchFragmentBinding
@@ -37,7 +34,7 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.button.setOnClickListener { cariTanaman() }
-        viewModel.getHasilTanaman().observe(requireActivity()){ updateUI(it)}
+//        viewModel.getHasilTanaman().observe(requireActivity()){ updateUI(it)}
     }
 
     fun cariTanaman() {
@@ -48,7 +45,7 @@ class SearchFragment : Fragment() {
         } else {
             showError(binding.tanamanHint, "")
             val pencarianIgnore = pencarian.toLowerCase()
-            viewModel.getTanaman(pencarianIgnore).observe(requireActivity()){
+            viewModel.getTanaman(pencarianIgnore).observe(requireActivity()) {
                 updateUI(it)
             }
         }
@@ -60,25 +57,18 @@ class SearchFragment : Fragment() {
     }
 
     private fun updateUI(filtered: TanamEntity?) {
-        if (filtered == null) {
-            showError(binding.tanamanHint, "Maaf data tidak ditemukan")
-            binding.details.isVisible = false
-            binding.judulTanaman.isVisible = false
-            binding.deskripsiTanaman.isVisible = false
-            binding.cara.isVisible = false
-            binding.namaLatin.isVisible = false
-            return
-        } else {
-            binding.details.isVisible = true
-            binding.judulTanaman.isVisible = true
-            binding.deskripsiTanaman.isVisible = true
-            binding.cara.isVisible = true
-            binding.namaLatin.isVisible = true
-            binding.details.text = context?.getString(R.string.details)
-            binding.judulTanaman.text = filtered.namaTanaman
-            binding.deskripsiTanaman.text = filtered.deskripsi
-            binding.cara.text = filtered.caraMerawat
-            binding.namaLatin.text = filtered.namaLatin
+        if (filtered == null)  {
+            return showError(binding.tanamanHint, "Data tidak ditemukan")
         }
+        binding.gambar.isVisible = true
+        binding.card.isVisible = true
+        binding.judulTanaman.isVisible = true
+        binding.deskripsiTanaman.isVisible = true
+        binding.cara.isVisible = true
+        binding.namaLatin.isVisible = true
+        binding.judulTanaman.text = filtered.namaTanaman
+        binding.deskripsiTanaman.text = filtered.deskripsi
+        binding.cara.text = filtered.caraMerawat
+        binding.namaLatin.text = filtered.namaLatin
     }
 }
