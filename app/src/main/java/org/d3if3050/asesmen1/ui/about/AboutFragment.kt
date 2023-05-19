f9f7793a3c0d5a3d30773d322cbf8a29096a8bfe
Yudhistira Haryatmaka Santoso <yudhistiraaaa20@gmail.com>
2023-05-20 00:07:57 +0700

    Menambahkan fitur intent ke aplikasi lain pada fragment about

--- app/src/main/java/org/d3if3050/asesmen1/ui/about/AboutFragment.kt
+++ app/src/main/java/org/d3if3050/asesmen1/ui/about/AboutFragment.kt
@@ -1,10 +1,10 @@
 package org.d3if3050.asesmen1.ui.about
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import org.d3if3050.asesmen1.R
 import org.d3if3050.asesmen1.databinding.AboutFragmentBinding
 
 class AboutFragment : Fragment() {
@@ -16,10 +16,31 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = AboutFragmentBinding.inflate(layoutInflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.about_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_bagi) {
+            shareApp()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun shareApp() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_SUBJECT, requireActivity().getString(R.string.app_name))
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Ayo dapatkan informasi lebih lanjut tentang tanaman di aplikasi Tanam"
+        )
+        requireActivity().startActivity(Intent.createChooser(intent, "Bagikan melalui"))
     }
 }
